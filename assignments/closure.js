f777afde55e57cbdf091dba26f96cbec57842ccf
patpalmerston <patpalmerston@gmail.com>
2019-02-26 16:25:11 -0800

    stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -27,10 +27,17 @@ myFunction();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function (){
+   return ++count;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
